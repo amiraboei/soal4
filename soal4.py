4c194063b8a4dad8236b1318d1555dd748e0b7a7
amiraboei <amiraboei1384@gmail.com>
2024-03-05 12:08:10 -0800

    soal4

---
+++ soal4.py
@@ -0,0 +1,9 @@
+n=int(input("dama ro brgo:"))
+print("age dama bar hasb F bod 1 ro bezan age bar hasb C bod 2 ro bezan:")
+w=int(input("kodom?"))
+if w==2:
+    b=(9/5)*n-32 
+    print("dama :",b)
+elif w==1:
+    b=(n+32)*5/9
+    print("dama :",b)
